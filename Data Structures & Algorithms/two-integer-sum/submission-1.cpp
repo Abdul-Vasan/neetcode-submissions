92b1734ba1ea76896ff68223440c91aec2f26ce8
Abdul Vasan <65154936+Abdul-Vasan@users.noreply.github.com>
2026-08-04 15:11:36 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // std::map<int, vector<int> > sumMap = {};
+        int sum;
+        for (int i = 0; i < nums.size(); i++){
+            for (int j = i+1; j < nums.size(); j++){
+                sum = nums[i] + nums[j];
+                // if (sumMap.find(sum) == sumMap.end()){
+                //     sumMap.insert({sum, std::vector<int>({i, j}) });
+                // }
+                if (sum == target){
+                    return std::vector<int>({i, j});
+                }
+            }
+        }
+
+        // return sumMap[target];
+
+    }
+};
